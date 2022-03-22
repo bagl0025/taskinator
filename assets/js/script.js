b790fc8a250d7a4338187315b1d1b44d9fce68fc
bagl0025 <bagl0025@umn.edu>
2022-03-22 14:15:35 -0500

    Capture Form Field Values

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,22 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function(event) {
     event.preventDefault();
-    
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    // create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+    
+    // create div item to hold task info and add item to list
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+
+    // add html content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+
+    // add entire item to list
     tasksToDoEl.appendChild(listItemEl);
 };
 
